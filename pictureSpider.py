f0055c97e2d9797d1247038a45cfa08ac7b014e1
unknown <>
2017-03-30 20:19:18 +0800

    First commit.

--- pictureSpider.py
+++ pictureSpider.py
@@ -1,7 +1,7 @@
 import requests
 root="F:/pictures/"
 base_url = "http://10.10.64.6:8080/Common/GetPhotoByBH?xh="
-for i in range(3150110001,3150931055):
+for i in range(1,2):
 	path = root + str(i)+ '.jpg'
 	url = base_url + str(i)+'.jpg'
 	r = requests.get(url)
